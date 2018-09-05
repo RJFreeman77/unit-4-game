d40dc336195ccfc0e3786e2b18055ef76577a62b
RyanJF <ryanjfreeman77@gmail.com>
2018-09-04 19:53:45 -0500

    making progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,8 @@
 // have functions inside objects that set the global stat variables to the character's variables
 
 // after enemy is defeated, change the div to display: none;
+
+// have to put down display information about who was attacked. 
 var playerCharacterHP = 0;
 var playerCharacterAP = 0;
 var enemyHP = 0;
@@ -19,25 +21,48 @@ var hotDogPrincess = {
     attackPoints: 3,
     counterAttack: 15,
     hasBattled: false,
+    id: $("#hot-dog"),
+    moveOtherCards: function () {
+        $("#opponents").append($("#choose-goose"));
+        $("#opponents").append($("#war-elephant"));
+        $("#opponents").append($("#james-i"));
+    },
 }
-
 var warElephant = {
     healthPoints: 200,
     attackPoints: 12,
     counterAttack: 30,
     hasBattled: false,
+    id: $("#war-elephant"),
+    moveOtherCards: function () {
+        $("#opponents").append($("#hot-dog"));
+        $("#opponents").append($("#choose-goose"));
+        $("#opponents").append($("#james-i"));
+    },
 }
 var chooseGoose = {
     healthPoints: 150,
     attackPoints: 8,
     counterAttack: 20,
     hasBattled: false,
+    id: $("#choose-goose"),
+    moveOtherCards: function () {
+        $("#opponents").append($("#hot-dog"));
+        $("#opponents").append($("#war-elephant"));
+        $("#opponents").append($("#james-i"));
+    },
 }
 var james = {
     healthPoints: 80,
     attackPoints: 5,
     counterAttack: 10,
     hasBattled: false,
+    id: $("#james-i"),
+    moveOtherCards: function () {
+        $("#opponents").append($("#hot-dog"));
+        $("#opponents").append($("#choose-goose"));
+        $("#opponents").append($("#war-elephant"));
+    },
 }
 
 function playerCharacerSelect() {
@@ -50,9 +75,9 @@ function PCselect() {
 
 $("div").click(function () {
     if (!isPCSelected && !isEnemySelected) {
-        switch ($(this).parents().attr("data")) {
+        switch ($(this).attr("data")) {
             case "hot-dog":
-                changeEnemyStat(hotDogPrincess);
+                changePCStat(hotDogPrincess);
                 console.log("set stats HDP")
                 break;
             case "choose-goose":
@@ -88,18 +113,21 @@ $("div").click(function () {
                 break;
         }
     }
-
-
 });
 
 
-var changePCStat = function (x) {
-    playerCharacterHP = x.healthPoints;
-    playerCharacterAP = x.attackPoints;
+var changePCStat = function (char) {
+    playerCharacterHP = char.healthPoints;
+    playerCharacterAP = char.attackPoints;
     isPCSelected = true;
-}
-var changeEnemyStat = function (x) {
-    enemyHP = x.healthPoints;
-    enemyCA = x.counterAttack;
+    char.moveOtherCards();
+    console.log("function called");
+};
+var changeEnemyStat = function (enem) {
+    enemyHP = enem.healthPoints;
+    enemyCA = enem.counterAttack;
     isEnemySelected = true;
-}
+    $("#npc-enemies").append(enem.id[0]);
+    console.log(enem.id[0]);
+};
+
